bcee4b38028d0947cf8856f42ad2576dddf8c932
Alex Karavil <46430499+Karavil@users.noreply.github.com>
2020-04-23 12:38:39 -0400

    Completed the enhancer module

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,34 @@
-module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
-};
+class Item {
+   constructor(name, durability, enhancement) {
+      this.name = name;
+      this.durability = durability;
+      this.enhancement = enhancement;
+   }
 
-function succeed(item) {
-  return { ...item };
+   succeed() {
+      if (this.enhancement < 20) this.enhancement += 1;
+   }
+
+   fail() {
+      if (this.enhancement > 16) this.enhancement -= 1;
+      else if (this.enhancement === 15) this.enhancement -= 10;
+      else this.enhancement -= 5;
+   }
+
+   repair() {
+      this.durability = 100;
+   }
+
+   get() {
+      let formattedName = this.name;
+      if (this.enhancement > 0) formattedName += ` [+${this.enhancement}]`;
+
+      return {
+         name: formattedName,
+         durability: this.durability,
+         enhancement: this.enhancement,
+      };
+   }
 }
 
-function fail(item) {
-  return { ...item };
-}
-
-function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
-}
+module.Item = Item;
